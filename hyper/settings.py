9c1bebd2ede6302858df935951274f9c9b6bfa5c
robert <robertandrei.oprescu@gmail.com>
2024-03-18 20:24:55 +0200

    fix 10

--- hyper/settings.py
+++ hyper/settings.py
@@ -100,8 +100,8 @@ DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Password validation
