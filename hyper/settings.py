02030088c11bc6260058ebdaaeb046ea831d9505
robert <robertandrei.oprescu@gmail.com>
2024-03-18 19:23:06 +0200

    Use Heroku DATABASE_URL for Postgres

--- hyper/settings.py
+++ hyper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,14 +88,15 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hypercrates',  # Change to your database name
-        'USER': 'admin',  # Change to your database username
-        'PASSWORD': 'admin',  # Change to your database password
-        'HOST': 'db',  # This should match the name of the database service in Docker Compose
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'hypercrates',  # Change to your database name
+    #     'USER': 'admin',  # Change to your database username
+    #     'PASSWORD': 'admin',  # Change to your database password
+    #     'HOST': 'db',  # This should match the name of the database service in Docker Compose
+    #     'PORT': '5432',
+    # }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
