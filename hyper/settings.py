eeb87dfbed020d938eb6265b56fca1b6f00c445a
robert <robertandrei.oprescu@gmail.com>
2024-03-18 19:28:15 +0200

    trying fix for collectstatic error

--- hyper/settings.py
+++ hyper/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,6 +99,9 @@ DATABASES = {
     # }
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Password validation
